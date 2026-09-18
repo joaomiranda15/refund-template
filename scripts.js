2af3f806beada63f9e3a326ddba7dd02a24c1350
Joao Paulo de Miranda Carvalho <paulocjpaulo@gmail.com>
2026-09-18 07:24:26 -0300

    Update refund template

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// Seleciona os elementos do formulário
+const amount = document.getElementById("amount");
+
+// Validação de campos no formulário.
+amount.oninput = () => {
+    // Obtém o valor atual do input e remove os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, "");
+
+    // Transforma o valor em centavos.
+    value = Number(value) / 100
+
+    // Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+// Formatação de moeda BRL
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+
+    return value;
+}
